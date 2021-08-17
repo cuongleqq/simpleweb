0a36dfc2278f010dba9c6e11da26716395726bc4
Cuong Le <cuongleqq@gmail.com>
2021-08-17 09:29:43 +0700

    Change API result

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send({result: "Hello World!"})
+  res.send({result: "Hello World2!"})
 })
 
 app.get('/secret', (req, res) => {
